fbb64a7d265e3d8ee5c81b85476a9867a6e541da
Austin Schrader <austin.jschrader@gmail.com>
2020-11-16 11:24:58 -0800

    Fix eslint error
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -2,20 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import { User } from './js/user.js';
 
 $(document).ready(function () {
   $('#formOne').submit(function (event) {
     event.preventDefault();
     let searchTerm = $('#user-search').val();
-    console.log(searchTerm);
 
-    // Their code
-    let apiRequest = $.get(
-      `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${process.env.API_KEY}&limit=1`
-    );
-    apiRequest.done(function (data) {
-      console.log('success got data', data);
-    });
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${process.env.API_KEY}&limit=1`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open('GET', url, true);
+    request.send();
+
+    function getElements(response) {
+      let gifEmbeddedUrl = response.data[0].embed_url;
+      console.log(gifEmbeddedUrl);
+      let gifUrl = response.data[0].id;
+      console.log(gifUrl);
+      // $('#results').text(`The gif is ${gifUrl}`);
+
+      $('#results').html(
+        `<iframe src="${gifEmbeddedUrl}" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><><a href="https://giphy.com/gifs/${gifUrl}"></a>`
+      );
+    }
   });
 });
